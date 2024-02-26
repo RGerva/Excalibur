c069df0794d4c8676e1a43b24455d1b56362ca7e
Renan Rodrigues Gervatosky <renan.rodrigues@gertec.com.br>
2024-02-26 15:43:01 -0300

    First Commit

--- src/main/java/br/com/excalibur/excaliburmod/Excalibur_Mod.java
+++ src/main/java/br/com/excalibur/excaliburmod/Excalibur_Mod.java
@@ -1,35 +1,37 @@
-package com.excaliburmod.excalibur_mod;
+package br.com.excalibur.excaliburmod;
 
-import com.excaliburmod.excalibur_mod.block.ExcaliburBlocks;
-import com.excaliburmod.excalibur_mod.block.entity.ExcaliburBlockEntities;
-import com.excaliburmod.excalibur_mod.creative.ExcaliburCreativeModTabs;
-import com.excaliburmod.excalibur_mod.item.ExcaliburItems;
-import com.excaliburmod.excalibur_mod.loot.ExcaliburLootModifiers;
-import com.excaliburmod.excalibur_mod.networking.ModMessages;
-import com.excaliburmod.excalibur_mod.recepie.ExcaliburRecipes;
-import com.excaliburmod.excalibur_mod.screen.ExcaliburMenuTypes;
-import com.excaliburmod.excalibur_mod.screen.GemInfusingStationScreen;
 import com.mojang.authlib.GameProfile;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
-
-import static net.minecraft.world.item.Items.registerItem;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Excalibur_Mod.MOD_ID)
@@ -39,7 +41,6 @@ public class Excalibur_Mod
     public static final String MOD_ID = "excalibur_mod";
     public static final String MOD_NAME = "Excalibur";
     public static final String LOG_TAG = '[' + MOD_NAME + ']';
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
     public static final GameProfile gameProfile = new GameProfile(UUID.nameUUIDFromBytes("excalibur.common".getBytes(StandardCharsets.UTF_8)), Excalibur_Mod.LOG_TAG);
 
@@ -47,14 +48,6 @@ public class Excalibur_Mod
 
         IEventBus modEventBus= FMLJavaModLoadingContext.get().getModEventBus();
 
-        ExcaliburCreativeModTabs.register(modEventBus);//Creative
-        ExcaliburItems.register(modEventBus);//Items
-        ExcaliburBlocks.register(modEventBus);//Blocks
-        ExcaliburLootModifiers.register(modEventBus);//Loot
-
-        ExcaliburBlockEntities.register(modEventBus);//Custom Entities
-        ExcaliburMenuTypes.register(modEventBus);//Menu Types
-        ExcaliburRecipes.register(modEventBus);//Custom Recepies
 
         modEventBus.addListener(this::commonSetup);
 
@@ -64,13 +57,11 @@ public class Excalibur_Mod
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(ModMessages::register);
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
-            event.accept(ExcaliburItems.EXCALIBUR_SWORD);
-            event.accept(ExcaliburItems.SAPPHIRE);
+
         }
     }
 
@@ -86,8 +77,6 @@ public class Excalibur_Mod
             LOGGER.info("CLIENT SETUP");
             LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
 
-            MenuScreens.register(ExcaliburMenuTypes.GEM_INFUSING_STATION_MENU.get(), GemInfusingStationScreen::new);
         }
     }
-
 }
